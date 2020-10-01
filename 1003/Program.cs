ae61dcc3395c29d99105acaafac16861d4125c1d
Bernard <bernardtelgon@gmail.com>
2020-10-01 14:14:19 -0300

    sum

---
+++ 1003/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, soma;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
---
+++ test2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, A, pi = 3.14159;
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A = pi * R * R;
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+
+
+           
+        }
+    }
+}
